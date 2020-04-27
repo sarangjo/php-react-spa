518dc9b3041a20e84d7bbd6071807a26cb11dd15
Sarang Joshi <sarangj@msn.com>
2020-04-27 00:26:10 -0700

    Updates

---
+++ build/precache-manifest.8d4773f272b73220032afc0dc64c9a0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d373aaf66bfab465b04cc8798843d0",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "e02770bc0c33b3e2ecaa",
+    "url": "/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bce651e505056f881bf0",
+    "url": "/build/static/js/2.d8a6d1b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build/static/js/2.d8a6d1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02770bc0c33b3e2ecaa",
+    "url": "/build/static/js/main.c9b73729.chunk.js"
+  },
+  {
+    "revision": "28a3a731337faf5f8283",
+    "url": "/build/static/js/runtime-main.17d5a8cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
